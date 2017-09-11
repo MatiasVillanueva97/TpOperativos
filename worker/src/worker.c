67cebe8f1fadba2e797105718965098a8696bc22
Nahuel Oyhanarte <noyhanarte@gmail.com>
2017-09-10 23:58:37 -0300

    Agregado worker template

---
+++ worker/src/worker.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : worker.c
+ Author      : Grupo 1234
+ Description : Proceso Worker
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <commons/config.h>
+#include <commons/string.h>
+
+#define PACKAGESIZE 1024	// Define cual va a ser el size maximo del paquete a enviar
+
+int main(int argc, char *argv[]) {
+
+	return 0;
+}
