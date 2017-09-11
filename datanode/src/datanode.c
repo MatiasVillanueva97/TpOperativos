314fbe8734ac4baa54b62b3396af295e21b6e811
Nahuel Oyhanarte <noyhanarte@gmail.com>
2017-09-10 23:58:28 -0300

    Agregado datanode template

---
+++ datanode/src/datanode.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : datanode.c
+ Author      : Grupo 1234
+ Description : Proceso DataNode
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <commons/config.h>
+#include <commons/string.h>
+
+#define PACKAGESIZE 1024	// Define cual va a ser el size maximo del paquete a enviar
+
+int main(int argc, char *argv[]) {
+
+	return 0;
+}
