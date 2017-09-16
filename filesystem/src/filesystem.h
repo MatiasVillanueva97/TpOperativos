a50dd53e44d28a988f9f8df651fa6936a44ea3d8
hernandomingo <hernan15>
2017-09-16 04:00:32 -0300

    agrego logs a filesystem

---
+++ filesystem/src/filesystem.h
@@ -0,0 +1,19 @@
+/*
+ * filesystem.h
+ *
+ *  Created on: 16/9/2017
+ *      Author: utnso
+ */
+
+#ifndef FILESYSTEM_SRC_FILESYSTEM_H_
+#define FILESYSTEM_SRC_FILESYSTEM_H_
+
+void crearLogger(){
+    char * archivoLog = strdup("FILESYSTEM_LOG.log");
+    logger = log_create("FILESYSTEM_LOG.log", archivoLog, true, LOG_LEVEL_INFO);
+    free(archivoLog);
+    archivoLog = NULL;
+}
+
+
+#endif /* FILESYSTEM_SRC_FILESYSTEM_H_ */
