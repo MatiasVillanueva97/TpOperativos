df67b521d7736cc0b32b7b672ed03dc19a307d7b
hernandomingo <hernan15>
2017-09-16 04:17:33 -0300

    agrego includes a filesystem.h

--- filesystem/src/filesystem.h
+++ filesystem/src/filesystem.h
@@ -8,6 +8,12 @@
 #ifndef FILESYSTEM_SRC_FILESYSTEM_H_
 #define FILESYSTEM_SRC_FILESYSTEM_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+t_log* logger;
+
 void crearLogger(){
     char * archivoLog = strdup("FILESYSTEM_LOG.log");
     logger = log_create("FILESYSTEM_LOG.log", archivoLog, true, LOG_LEVEL_INFO);
