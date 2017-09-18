c7eba49b58129dc7a99de662e1ddddcae62f0546
Guillermo Fernandez <gafernandez2525@gmail.com>
2017-09-18 00:14:50 -0300

    refactor y agregados a utils.h y utils.c

--- utils/utils.c
+++ utils/utils.c
@@ -7,6 +7,7 @@
 
 #define MAX_CONEXIONES 10
 
+/* ******************** funciones para archivos de configuración ************************ */
 int leerArchivoConfig(char *pathArchivoConfig, char **keysConfig, char **datosConfig) {
 	int i;
 	t_config *archivoConfig = config_create(pathArchivoConfig);
@@ -23,6 +24,7 @@ int leerArchivoConfig(char *pathArchivoConfig, char **keysConfig, char **datosCo
 	return EXIT_SUCCESS;
 }
 
+/* ******************** funciones para clientes de conexiones ************************ */
 int conectarA(char *IP, char *puerto){
 	// Uso getaddrinfo() para obtener los datos de la direccion de red y guardarlos en serverInfo.
 	struct addrinfo hints;
@@ -34,23 +36,30 @@ int conectarA(char *IP, char *puerto){
 	getaddrinfo(IP, puerto, &hints, &serverInfo);	// Carga en serverInfo los datos de la conexion
 
 	// Obtengo un socket, utilizando la estructura serverInfo ya generada.
-	int socketClient = socket(serverInfo->ai_family,serverInfo->ai_socktype, serverInfo->ai_protocol);
+	int socketCliente = socket(serverInfo->ai_family,serverInfo->ai_socktype, serverInfo->ai_protocol);
 
-	if(socketClient<0){
+	if(socketCliente<0){
 		perror("socket");
 		return -1;
 	}
 
 	// Me conecto al server usando el file descriptor del socket previo.
-	if(connect(socketClient, serverInfo->ai_addr, serverInfo->ai_addrlen)<0){
+	if(connect(socketCliente, serverInfo->ai_addr, serverInfo->ai_addrlen)<0){
 		perror("connect");
 		return -1;
 	}
 	freeaddrinfo(serverInfo);	// No lo necesitamos mas
 
-	return 0;
+	return socketCliente;
 }
 
+void cerrarClient(int socket){
+	// Cierro la conexión y libero el socket
+	close(socket);
+	return;
+}
+
+/* ******************** funciones para servers de conexiones ************************ */
 int inicializarServer(char *IP, char *puerto) {
 	// Uso getaddrinfo() para obtener los datos de la direccion de red y guardarlos en serverInfo.
 	struct addrinfo hints;
@@ -63,51 +72,45 @@ int inicializarServer(char *IP, char *puerto) {
 	getaddrinfo(NULL, puerto, &hints, &serverInfo); // Notar que le pasamos NULL como IP, ya que le indicamos que use localhost en AI_PASSIVE
 
 	// Obtengo un socket, utilizando la estructura serverInfo ya generada.
-	int socketServer = socket(serverInfo->ai_family,serverInfo->ai_socktype, serverInfo->ai_protocol);
+	int listenningSocket = socket(serverInfo->ai_family,serverInfo->ai_socktype, serverInfo->ai_protocol);
 
-	if(socketServer<0){
+	if(listenningSocket<0){
 		perror("socket");
 		return -1;
 	}
 
 	// Uso el file descriptor previo para decirle al sistema por donde voy a escuchar las conexiones
-	if(bind(socketServer, serverInfo->ai_addr,serverInfo->ai_addrlen)!=0){
+	if(bind(listenningSocket, serverInfo->ai_addr,serverInfo->ai_addrlen)!=0){
 		perror("bind");
 		return -1;
 	}
 	freeaddrinfo(serverInfo); // Ya no lo vamos a necesitar
 
 	// Empiezo a escuchar las conexiones. IMPORTANTE: listen() es una syscall BLOQUEANTE.
-	if(listen(socketServer, MAX_CONEXIONES)!=0){
+	if(listen(listenningSocket, MAX_CONEXIONES)!=0){
 		perror("listen");
 		return -1;
 	}
 
-	return 0;
+	return listenningSocket;
 }
 
-void cerrarClient(int socket){
-	// Cierro la conexión y libero el socket
-	close(socket);
-	return;
+int aceptarConexion(int listenningSocket) {
+	int socketCliente;
+	// Acepto la conexión del cliente y abro el *nuevo* socket
+	struct sockaddr_in addr; // Esta estructura contendra los datos de la conexion del cliente. IP, puerto, etc.
+	socklen_t addrlen = sizeof(addr);
+	if((socketCliente = accept(listenningSocket, (struct sockaddr *) &addr,&addrlen))<0){
+		perror("accept");
+		return -1;
+	}
+	return socketCliente;
 }
 
-//int aceptarConexion(datosConfigServer *datosConexionServer) {
-//	// Acepto la conexión del cliente y abro el *nuevo* socket
-//	struct sockaddr_in addr; // Esta estructura contendra los datos de la conexion del cliente. IP, puerto, etc.
-//	socklen_t addrlen = sizeof(addr);
-//	if((datosConexionServer->socketCliente = accept(datosConexionServer->listenningSocket, (struct sockaddr *) &addr,&addrlen))<0){
-//		perror("accept");
-//		return -1;
-//	}
-//	return 0;
-//}
-
-void cerrarServer(int socket) {
-
+void cerrarServer(int listenningSocket) {
 	// Cierro la conexión y libero el socket
-	close(socket);
-	//close(datosConexionServer->listenningSocket);
+	//close(socketCliente);
+	close(listenningSocket);
 	printf("\nCliente desconectado.");
 	return;
 }
--- utils/utils.h
+++ utils/utils.h
@@ -1,14 +1,56 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 
+/* ******************** funciones para clientes de conexiones ************************ */
+
+/*
+ * lee el archivo de configuración y guarda los datos en un array
+ * recibe el path del archivo de configuración, el puntero al array con las keys a leer y el puntero al array donde guardar los datos
+ * devuelve 0 si OK o 1 si ERROR
+ */
 int leerArchivoConfig(char *pathArchivoConfig, char **keysConfig, char **datosConfig);
 
+/* ******************** funciones para clientes de conexiones ************************ */
+
+/*
+ * conecta un cliente a un server
+ * recibe la ip y el puerto del server
+ * devuelve el número de socket si OK o -1 si ERROR
+ */
 int conectarA(char *IP, char * puerto);
 
-int inicializarServer(char *IP, char * puerto);
-
+/*
+ * Cierra la conexión y libera el socket
+ * recibe el socket
+ */
 void cerrarClient(int socket);
 
-void cerrarServer(int socket);
+/* ******************** funciones para servers de conexiones ************************ */
+
+/*
+ * inicializa el server y lo pone a escuchar (función bloqueante por la syscall bloqueante "listen")
+ * recibe la ip y el puerto por el que va a quedar escuchando el server
+ * devuelve el socket a través del cual escuchará nuevas conexiones si OK o -1 si ERROR
+ */
+int inicializarServer(char *IP, char * puerto);
+
+/*
+ * Acepta la conexión del cliente y abre el *nuevo* socket
+ * recibe el socket a través del cual el server está escuchando nuevas conexiones
+ * devuelve el nuevo socket a través del cual va a continuar la comunicación con el cliente
+ */
+int aceptarConexion(int listenningSocket);
+
+/*
+ * Cierra la conexión y libera el socket
+ * recibe el socket
+ */
+void cerrarClient(int socket);
+
+/*
+ * Cierra la conexión y libera el socket
+ * recibe el socket de escucha
+ */
+void cerrarServer(int listenningSocket);
 
 #endif /* UTILS_H_ */
