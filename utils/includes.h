a72c394f86c946d4aaf8ed12d16d907bfe3cf92a
Guillermo Fernandez <gafernandez2525@gmail.com>
2017-09-25 14:49:33 -0300

    header con los includes que se usan en todos los archivos

---
+++ utils/includes.h
@@ -0,0 +1,25 @@
+/*
+ * includes.h
+ *
+ *  Created on: 24/9/2017
+ *      Author: utnso
+ */
+
+#ifndef UTILS_INCLUDES_H_
+#define UTILS_INCLUDES_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <commons/config.h>
+#include <commons/string.h>
+#include <commons/log.h>
+#include "utils.h"
+#include "conexionesSocket.h"
+#include "archivoConfig.h"
+#include "constantes.h"
+#include "comunicacion.h"
+
+
+#endif /* UTILS_INCLUDES_H_ */
