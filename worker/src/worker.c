db5bcdea2a8ffba8a6053c052cd86045fe4f1208
hernandomingo <hernan15>
2017-11-10 06:21:57 -0300

    Subo lo que tengo de worker. Falta: 1- ver como se conecta con Master y como pasa los parametros. 2- Hacer que se conecte con otros workers para etapa de reduccion global. 3- Revisar todas las cosas comentadas que tengo que cambiar

--- worker/src/worker.c
+++ worker/src/worker.c
@@ -15,6 +15,10 @@
 #include "../../utils/conexionesSocket.h"
 #include "../../utils/archivoConfig.h"
 
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
 //#define PACKAGESIZE 1024	// Define cual va a ser el size maximo del paquete a enviar
 
 enum keys {
@@ -25,19 +29,36 @@ char* datosConfigWorker[2];
 
 // ================================================================ //
 // Worker es el que realiza las operaciones que le pide el Master.
-// 1) Recibe orden del Master
-// 2) Se forkea
-// 3) El principal sigue escuchando, el fork ejecuta la orden
-// 4) Termina la orden y el fork muere
+// 1) Lee archivo de configuracion del nodo.
+// 2) Espera conexion de Masters.
+// 3) Recibe conexion del Master y se forkea
+// 4) El principal sigue escuchando, el fork ejecuta la orden
+// 5) Termina la orden, aviso a master que terminó y el fork muere
 // Puede haber varios Worker corriendo al mismo tiempo.
 // ================================================================ //
 
 int main(int argc, char *argv[]) {
+
+    #define SIZE 1024 //tamaño para comunicaciones entre padre e hijos
+    //tamanioData = stat --format=%s "nombre archivo" //tamaño data.bin en bytes
+
 	t_log* logWorker;
 	logWorker = log_create("logFile.log", "WORKER", false, LOG_LEVEL_TRACE); //creo el logger, sin mostrar por pantalla
 
 	log_info(logWorker, "Iniciando Worker");
 	printf("\n*** Proceso worker ***\n");
+
+	//ARREGLAR CON NAHUEL COMO SE VAN A PASAR LOS PARAMETROS, NO ESTOY SEGURO SI TIENE QUE INICIAR YA CON PARAMETROS O INICIAR
+	//SIN PARAMETROS Y DESPUES RECIBIR POR SOCKET
+    /*if (argc < 4) {
+        puts("Error. Faltan parámetros en la ejecución del proceso.\n");
+        return EXIT_FAILURE;
+    }
+
+    char *codigo = argv[1];
+    char *origen = argv[2];
+    char *destino = argv[3];
+    */
 
 	// 1º) leer archivo de config.
 	char *nameArchivoConfig = "configNodo.txt";
@@ -47,6 +68,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	// 2º) inicializar server y aguardar conexiones (de master)
+	//HAY QUE VER COMO SE CONECTA CON OTROS WORKERS
 	int listenningSocket = inicializarServer(datosConfigWorker[IP_PROPIA], datosConfigWorker[PUERTO_PROPIO]);
 	if (listenningSocket < 0) {
 		log_error(logWorker, "No pude iniciar como servidor");
@@ -65,6 +87,67 @@ int main(int argc, char *argv[]) {
 
 		log_info(logWorker, "Worker conectado, esperando mensajes");
 		puts("Ya me conecté, ahora estoy esperando mensajes\n");
+
+		int pipe_padreAHijo[2]; //PIPES: 0 lectura, 1 escritura
+		int pipe_hijoAPadre[2];
+
+		pipe(pipe_padreAHijo);
+		pipe(pipe_hijoAPadre);
+
+		pid_t pid;
+		int status;
+		char* buffer=malloc(SIZE);
+
+		if ((pid=fork()) == 0 ) //aca ya se hizo el proceso hijo
+		{
+		    dup2(pipe_padreAHijo[0],STDIN_FILENO);
+		    dup2(pipe_hijoAPadre[1],STDOUT_FILENO);
+
+		    // Cerramos pipes duplicados
+		    close(pipe_padreAHijo[1]);
+		    close(pipe_hijoAPadre[0]);
+		    close(pipe_hijoAPadre[1]);
+		    close(pipe_padreAHijo[0]);
+
+
+		    char *argv[] = {NULL};
+		    char *envp[] = {NULL};
+		    //ACA CORRER CON SYSTEM --> ej:   system("echo hola pepe | ./script_transformacion.py > /tmp/resultado");
+		    //execve("./script_transformacion.py", argv, envp);
+		    exit(1);
+
+		} //aca termina el hijo
+
+		else {
+		    close(pipe_padreAHijo[0]); //Lado de lectura de lo que el padre le pasa al hijo.
+		    close(pipe_hijoAPadre[1]); //Lado de escritura de lo que hijo le pasa al padre.
+
+		    /*REVISAR
+		    write(pipe_padreAHijo[1],argv[1],strlen(argv[1])); //codigo
+		    write(pipe_padreAHijo[1],argv[2],strlen(argv[2])); //origen
+		    write(pipe_padreAHijo[1],argv[3],strlen(argv[3])); //destino
+		    */
+
+		    close(pipe_padreAHijo[1]);
+		    waitpid(pid,&status,0);
+
+		    read(pipe_hijoAPadre[0], buffer, SIZE);
+		    close(pipe_hijoAPadre[0]);
+
+		}
+
+		//FILE* fd = fopen("/tmp/resultado","w"); //ESCRIBIR RESULTADO
+		//fputs(buffer,fd);
+		//fclose(fd);
+
+
+		free(buffer);
+		return 0;
+
+
+
+
+
 
 		// 3º) creo forks para ejecutar instrucciones de master
 //		int pid = fork();
