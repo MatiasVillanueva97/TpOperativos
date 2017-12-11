80627d54249256150233a64fddc365be68168bc1
hernandomingo <hernan15>
2017-12-11 06:36:25 -0300

    agregue varios free

--- datanode/src/datanode.c
+++ datanode/src/datanode.c
@@ -22,7 +22,7 @@ enum keys {
 	IP_PROPIA, PUERTO_PROPIO, FS_IP, FS_PUERTO, RUTA_DATABIN, NOMBRE_NODO
 };
 char* keysConfigDataNode[] = { "IP_PROPIA", "PUERTO_PROPIO", "FS_IP", "FS_PUERTO", "RUTA_DATABIN", "NOMBRE_NODO",
-NULL };
+		NULL };
 
 //FILE* archivo;
 char* datosConfigDataNode[6];
@@ -31,6 +31,7 @@ t_log* logDataNode;
 void *mapArchivo;
 struct stat buff;
 int fd;
+int unMega = 1048576;
 
 int conexionAFileSystem() {
 	log_trace(logDataNode, "Conexión a FileSystem, IP: %s, Puerto: %s", datosConfigDataNode[FS_IP], datosConfigDataNode[FS_PUERTO]);
@@ -47,7 +48,7 @@ int conexionAFileSystem() {
 void setBloque(int idBloque, char* datos) {
 	if (idBloque < tamanoArchivo) {
 		int bytesEscritos = sizeof(char*) * strlen(datos) + 1;
-		int posicion = idBloque * 1048576;
+		int posicion = idBloque * unMega;
 		mapArchivo = mmap(0, buff.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, posicion);
 		if (mapArchivo == MAP_FAILED) {
 			perror("mmap");
@@ -67,15 +68,15 @@ char* getBloque(int idBloque) {
 		printf("El bloque %i no existe en este nodo\n", idBloque);
 		return 0;
 	}
-	char *buffer = malloc(1048576);
-	int posicion = idBloque * 1048576;
-	mapArchivo = mmap(0, 1048576, PROT_READ, MAP_SHARED, fd, posicion);
+	char *buffer = malloc(unMega);
+	int posicion = idBloque * unMega;
+	mapArchivo = mmap(0, unMega, PROT_READ, MAP_SHARED, fd, posicion);
 	if (mapArchivo == MAP_FAILED) {
 		perror("mmap");
 		close(fd);
 		exit(1);
 	}
-	strncpy(buffer, mapArchivo, 1048576);
+	strncpy(buffer, mapArchivo, unMega);
 	log_info(logDataNode, "bytes leidos en el bloque %i\n", idBloque);
 	printf("bytes leidos en el bloque %i\n", idBloque);
 	//printf("%s\n",buffer);
@@ -88,14 +89,14 @@ int cantidadBloquesAMandar(char * PATH) {
 	int cantidadBloques = 0;
 	fseek(archivo, 0, SEEK_END);
 	tamanoArchivo = ftell(archivo);
-	cantidadBloques = tamanoArchivo / 1048576;
+	cantidadBloques = tamanoArchivo / unMega;
 	return cantidadBloques;
 }
 
 void pruebas2(int socket, char **datosConfigDataNode) {
 	char* nombre = datosConfigDataNode[5];
-	char * ip = datosConfigDataNode[0];
-	char * puerto = datosConfigDataNode[1];
+	char* ip = datosConfigDataNode[0];
+	char* puerto = datosConfigDataNode[1];
 	int cantidadBloq = cantidadBloquesAMandar(datosConfigDataNode[4]);
 
 	int cantStrings = 4;
@@ -134,6 +135,7 @@ void pruebas2(int socket, char **datosConfigDataNode) {
 	int bytesEnviados = enviarMensaje(socket, mensajeSerializado);
 
 }
+
 int main(int argc, char *argv[]) {
 
 	logDataNode = log_create("logDataNode.log", "DataNode", true, LOG_LEVEL_TRACE); //creo el logger, mostrando por pantalla
@@ -156,7 +158,7 @@ int main(int argc, char *argv[]) {
 	 int fd = fileno(archivo);
 	 struct stat buff;
 	 fstat(fd, &buff);
-	 tamanoArchivo = buff.st_size / 1048576;
+	 tamanoArchivo = buff.st_size / unMega;
 	 */
 	fd = open(datosConfigDataNode[4], O_RDWR);
 	if (fd == -1) {
@@ -169,7 +171,7 @@ int main(int argc, char *argv[]) {
 		exit(1);
 	}
 	//tamaño de archivo data.bin
-	tamanoArchivo = buff.st_size / 1048576;
+	tamanoArchivo = buff.st_size / unMega;
 
 	//3°) me conecto al FS y espero solicitudes
 	int socketFS; //= conectarA("127.0.0.1","5000");
@@ -192,17 +194,19 @@ int main(int argc, char *argv[]) {
 			char** arrayMensajesRecibidos = deserializarMensaje(socketFS, cantMensajes);
 			setBloque(atoi(arrayMensajesRecibidos[1]), arrayMensajesRecibidos[0]);
 			log_trace(logDataNode, "Setie bloque. Numero de bloque %s, Tipo archivo %s, Recibi mensaje %d", arrayMensajesRecibidos[1], arrayMensajesRecibidos[2], j);
+			liberar_array(arrayMensajesRecibidos, cantMensajes);
 			j++;
 		}
-			break;
+		break;
 
 		case TIPO_MSJ_PEDIR_BLOQUES: {
 			//piden bloques y los mando
 			int cantMensajes = protocoloCantidadMensajes[header];
 			char** arrayMensajesRecibidos = deserializarMensaje(socketFS, cantMensajes);
 			log_trace(logDataNode, "bloque %d", atoi(arrayMensajesRecibidos[0]));
-			char * buffer = getBloque(atoi(arrayMensajesRecibidos[0]));
-			munmap(mapArchivo, 1048576);
+			char* buffer = getBloque(atoi(arrayMensajesRecibidos[0]));
+			liberar_array(arrayMensajesRecibidos, cantMensajes);
+			munmap(mapArchivo, unMega);
 
 			log_info(logDataNode, "Paso getBloque");
 
@@ -220,14 +224,16 @@ int main(int argc, char *argv[]) {
 				log_error(logDataNode, "error de malloc 1");
 			}
 			strcpy(arrayMensajesSerializarEnviar[i], buffer);
+			free(buffer);
 			i++;
 
 			char *mensajeSerializado = serializarMensaje(TIPO_MSJ_BLOQUE_DESDE_DATANODE, arrayMensajesSerializarEnviar, cantStrings1);
 			//printf("mensajeSerializado : %s\n", mensajeSerializado);
 			int bytesEnviados = enviarMensaje(socketFS, mensajeSerializado);
+			liberar_array(arrayMensajesSerializarEnviar, cantStrings1);
 			log_trace(logDataNode, "Bytes enviados: %d", bytesEnviados);
 		}
-			break;
+		break;
 
 		default: {
 			log_error(logDataNode, "Se cayo FileSystem");
@@ -236,7 +242,7 @@ int main(int argc, char *argv[]) {
 
 		}
 
-			break;
+		break;
 		}
 	}
 
